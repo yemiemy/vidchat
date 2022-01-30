30ed88c45af881d000283a288edbb88ed94a9b73
Yemiemy <rasholayemi@gmail.com>
2022-01-30 15:37:07 +0100

    Adding controls

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,5 +1,5 @@
 const APP_ID = '9a50aed5dc584f3bb27c5bd40e4a69ce'
-const TOKEN = "0069a50aed5dc584f3bb27c5bd40e4a69ceIAB/w140XJoppDzz1sHbsnEKB3BxIMxKhC9s8VZ1zaaKwmTNKL8AAAAAEABLPQ3JWNXtYQEAAQBX1e1h"
+const TOKEN = "0069a50aed5dc584f3bb27c5bd40e4a69ceIACLV1VEetMEGam1TN3yrs0Coaso23lpRI//O169cxXCH2TNKL8AAAAAEADbqsx0z9b2YQEAAQDO1vZh"
 const CHANNEL = "main"
 
 const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" })
@@ -54,4 +54,43 @@ let handleUserJoined = async (user, mediaType) => {
     }
 }
 
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async () => {
+    for (let i = 0; localTracks.length > i; i++){
+        localTracks[i].stop()
+        localTracks[i].close()
+    }
+
+    await client.leave()
+    window.open('/', '_self')
+}
+
+let toggleMic = async (e) => {
+    if(localTracks[0].muted){
+        await localTracks[0].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }else{
+        await localTracks[0].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
+
 joinAndDisplayLocalStream()
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('video-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
